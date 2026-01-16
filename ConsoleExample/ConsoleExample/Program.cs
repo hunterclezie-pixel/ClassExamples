f1972c38ba6a6b074aa93c4643a8c9a1fae810e3
Hunter Clezie <clezhunt@gmail.com>
2026-01-16 08:32:05 -0700

    Add Header

--- ConsoleExample/ConsoleExample/Program.cs
+++ ConsoleExample/ConsoleExample/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace ConsoleExample
+﻿/* 
+Hunter Clezie 
+Spring 2026
+RCET2265
+Console Practice program
+github url: https://github.com/hunterclezie-pixel/ClassExamples.git
+*/
+namespace ConsoleExample
 {
     internal class Program
     {
