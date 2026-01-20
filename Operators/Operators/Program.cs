60aaa8765703812516e450425f3e7f7226d5da16
Hunter Clezie <clezhunt@gmail.com>
2026-01-20 08:18:05 -0700

    add sample arithmatic functions

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine(5+5);
+            Console.WriteLine(5-3);
+            Console.WriteLine(5*2);
+            Console.WriteLine(13/5); //integer
+            Console.WriteLine(13%5); //remainder
+            Console.WriteLine(13d / 5d); //floating point decimal
+            Console.WriteLine(13.0 / 5.0) //floating point decimal
+
+
+            //pause
+            Console.Read();
         }
     }
 }
