bbddfaa7130fa419935030c7eca73406c14a2e05
Hunter Clezie <clezhunt@gmail.com>
2026-01-21 08:39:25 -0700

    Reflect user input

--- ConsoleExample/ConsoleExample/Program.cs
+++ ConsoleExample/ConsoleExample/Program.cs
@@ -11,11 +11,17 @@ namespace ConsoleExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.Beep(1000,500);
-            Console.Beep(500, 500);
-            Console.Beep(700, 500);
-            Console.Beep(300, 500);
+            //Console.WriteLine("Hello, World!");
+            //Console.Beep(1000,500);
+
+            string userinput = "Hello";
+
+            Console.WriteLine("Please Type Some Text");
+            userinput = Console.ReadLine();
+
+            Console.WriteLine($"You entered:\"{userinput}\""); //Scaping is using the backslashes, formatted 
+            Console.WriteLine("You entered: " + "\"" + userinput + "\""); //Concatnated String
+
             Console.Read();
         }
     }
