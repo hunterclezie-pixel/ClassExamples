39923ec6be111e9f2e7d898e45ce237a142b58d7
Hunter Clezie <clezhunt@gmail.com>
2026-01-26 08:30:09 -0700

    Simple exceptions

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userinput = "";
+            int result = 0;
+
+            Console.WriteLine("Please Enter a Number");
+            userinput = Console.ReadLine();
+            Console.WriteLine($"You enter {userinput}");
+
+            try
+            {
+                // risky code
+            result = int.Parse(userinput);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong: " + ex.StackTrace);
+            }
+
+            //pause
+            Console.ReadLine();
+        }
+    }
+}
