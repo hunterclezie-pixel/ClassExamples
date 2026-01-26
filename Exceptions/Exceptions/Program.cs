b597f739273ab95bf39a9576aa4945114436effe
Hunter Clezie <clezhunt@gmail.com>
2026-01-26 08:54:07 -0700

    loop init values until true or false

--- Exceptions/Exceptions/Program.cs
+++ Exceptions/Exceptions/Program.cs
@@ -6,21 +6,43 @@
         {
             string userinput = "";
             int result = 0;
+            bool isValid = false;
+            string message = "";
 
-            Console.WriteLine("Please Enter a Number");
-            userinput = Console.ReadLine();
-            Console.WriteLine($"You enter {userinput}");
+            do
+            {
+                Console.WriteLine("Please Enter a Number");
+                Console.WriteLine("Enter \"Q\" to exit");
+                userinput = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine($"You enter {userinput}");
 
-            try
-            {
-                // risky code
-            result = int.Parse(userinput);
-                
+                try
+                {
+                    // risky code
+                result = int.Parse(userinput);
+                isValid = true;
+                message = $"Good job! {result} is a number!";
+
+                }
+                catch (Exception ex)
+                {
+                    if (userinput == "Q"|| userinput == "q")
+                    {
+                        message = "Have a nice day!";
+                        isValid = true;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"{userinput} is not a number");
+                    }
+                }
+
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Something went wrong: " + ex.StackTrace);
-            }
+            while (isValid == false);
+
+            message= $"Good job! {result} is a number!";
 
             //pause
             Console.ReadLine();
