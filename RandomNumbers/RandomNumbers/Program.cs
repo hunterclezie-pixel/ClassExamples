3d2d93140d3ccc37eac6290961be11a4f9273c94
Hunter Clezie <clezhunt@gmail.com>
2026-01-27 08:32:23 -0700

    init

---
+++ RandomNumbers/RandomNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RandomNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random(); //Constructor
+            int n = rand.Next(1, 7); // Random number between 1 and 6
+            Console.WriteLine(n); 
+
+            //pause
+            Console.Read();
+        }
+    }
+}
