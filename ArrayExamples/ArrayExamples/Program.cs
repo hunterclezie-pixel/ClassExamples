603ad2024f9728461213accb5c616bef9acb71bc
Hunter Clezie <clezhunt@gmail.com>
2026-01-28 08:30:51 -0700

    add array exaple

---
+++ ArrayExamples/ArrayExamples/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ArrayExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int thing = 0;
+            string[] fruits = new string[7];
+            fruits[0] = "apple";
+            fruits[1] = "banana";
+            fruits[2] = "Kiwi";
+            fruits[3] = "tomato";
+            fruits[4] = "Pineapple";
+            fruits[5] = "Dragon Fruit";
+            fruits[6] = "Watermelon";
+            //fruits[7] = "Tangerine"; //this will crash. index out of range
+
+            Console.WriteLine(fruits[3]);
+
+
+            for (int i = 0; i < 7; i++)
+            { 
+                Console.WriteLine(fruits[i]);
+            }
+
+            // Same with no index or need for known length
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+ 
+            //Pause
+            Console.Read();
+        }
+    }
+}
--- RandomNumbers/RandomNumbers/Program.cs
+++ RandomNumbers/RandomNumbers/Program.cs
@@ -4,9 +4,17 @@
     {
         static void Main(string[] args)
         {
+
             Random rand = new Random(); //Constructor
-            int n = rand.Next(1, 7); // Random number between 1 and 6
-            Console.WriteLine(n); 
+
+            //test randomness and range with a for loop
+            for (int i = 0; i < 1000; i++)
+            {
+                //rand.Next(min,max) max is not inclusive
+                int n = rand.Next(1, 7); // Random number between 1 and 6
+                
+                Console.Write(n);
+            }
 
             //pause
             Console.Read();
