9585acd8ee6260240c3fd86e5a64e93661acc5c8
Hunter Clezie <clezhunt@gmail.com>
2026-01-28 08:51:21 -0700

    add colum and row

--- ArrayExamples/ArrayExamples/Program.cs
+++ ArrayExamples/ArrayExamples/Program.cs
@@ -6,33 +6,46 @@
         {
             int thing = 0;
             string[] fruits = new string[7];
-            fruits[0] = "apple";
-            fruits[1] = "banana";
-            fruits[2] = "Kiwi";
-            fruits[3] = "tomato";
-            fruits[4] = "Pineapple";
-            fruits[5] = "Dragon Fruit";
-            fruits[6] = "Watermelon";
-            //fruits[7] = "Tangerine"; //this will crash. index out of range
-
-            Console.WriteLine(fruits[3]);
+            string[] daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday", "Thursday","Friday","Saturday"];
+            int[,] grid = new int[5,5];
 
 
-            for (int i = 0; i < 7; i++)
-            { 
-                Console.WriteLine(fruits[i]);
-            }
+            //fruits[0] = "apple";
+            //fruits[1] = "banana";
+            //fruits[2] = "Kiwi";
+            //fruits[3] = "tomato";
+            //fruits[4] = "Pineapple";
+            //fruits[5] = "Dragon Fruit";
+            //fruits[6] = "Watermelon";
+            ////fruits[7] = "Tangerine"; //this will crash. index out of range
 
-            // Same with no index or need for known length
-            foreach (string fruit in fruits)
+            //Console.WriteLine(fruits[3]);
+
+
+            //for (int i = 0; i < 7; i++)
+            //{ 
+            //    Console.WriteLine(fruits[i]);
+            //}
+
+            //// Same with no index or need for known length
+            //foreach (string fruit in fruits)
+            //{
+            //    Console.WriteLine(fruit);
+            //}
+
+            //Console.WriteLine($"The length of the fruits is: {fruits.Length}");
+
+            for (int column = 0; column < 5; column++)
             {
-                Console.WriteLine(fruit);
+                for (int row = 0; row < 5; row++)
+                {
+                    grid[column, row] = column * row;
+                }
             }
+                    Console.WriteLine(grid[3,3]);
 
-            Console.WriteLine($"The length of the fruits is: {fruits.Length}");
-
-            //Pause
-            Console.Read();
+                //Pause
+                Console.Read();
         }
     }
 }
