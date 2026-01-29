3430c48ba89118d8dae0317e35f49acb5dbc70a4
Hunter Clezie <clezhunt@gmail.com>
2026-01-29 08:50:19 -0700

    Add method usages

--- MethodExample/MethodExample/Program.cs
+++ MethodExample/MethodExample/Program.cs
@@ -4,17 +4,39 @@
     {
         static void Main(string[] args)
         {
-
-            SayHello();
-
+            int count = 5;
+            //SayHello();
+            //AddNumbers();
+            //AddNumbers();
+            //SayHello();
+            Console.WriteLine(count);
+            count = SumNumber(5, 7);
+            Console.WriteLine(count);
             //pause
             Console.Read();
         }
 
-        static void SayHello() 
-        
-        { 
+        static void SayHello()
+
+        {
             Console.WriteLine("Hello, World!");
+        }
+
+        static void AddNumbers()
+
+        {
+            Console.WriteLine(1 + 1);
+        }
+
+        static void ShowCount()
+        {
+            int count = 7;
+            Console.WriteLine(count);
+        }
+
+        static int SumNumber(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
         }
     }
 }
