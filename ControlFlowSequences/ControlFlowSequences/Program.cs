1a2ce0764b4f2c52ab3c4a35b08b4871a56aa042
Hunter Clezie <clezhunt@gmail.com>
2026-01-30 08:57:13 -0700

    Quit Message

--- ControlFlowSequences/ControlFlowSequences/Program.cs
+++ ControlFlowSequences/ControlFlowSequences/Program.cs
@@ -16,106 +16,114 @@ namespace ControlFlowSequences
             bool userQuit = false, validResponse = false;
             string userinput = "";
 
-            //First Number
             do
             {
-                Console.WriteLine("Please enter a number");
-                userinput = Console.ReadLine();
-                try
+                //First Number
+                do
                 {
-                    Console.WriteLine($"you entered {int.Parse(userinput)}");
-                    validResponse = true;
-                }
-                catch (Exception)
+                    Console.WriteLine("Please enter a number");
+                    userinput = Console.ReadLine();
+                    try
+                    {
+                        Console.WriteLine($"you entered {int.Parse(userinput)}");
+                        validResponse = true;
+                    }
+                    catch (Exception)
+                    {
+                        if (userinput == "Q" || userinput == "q")
+                        {
+                            userQuit = true;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine($"{userinput} is not a number");
+                        }
+
+                    }
+                } while (validResponse == false && userQuit == false);
+
+                Console.WriteLine("end of first loop");
+
+                validResponse = false; //reset flag
+                                       //Second number
+                while (validResponse == false && userQuit == false)
                 {
-                    if (userinput == "Q" || userinput == "q")
+                    Console.WriteLine("Please enter a number");
+                    userinput = Console.ReadLine();
+                    try
                     {
-                        userQuit = true;
+                        Console.WriteLine($"you entered {int.Parse(userinput)}");
+                        validResponse = true;
                     }
+                    catch (Exception)
+                    {
+                        if (userinput == "Q" || userinput == "q")
+                        {
+                            userQuit = true;
+                        }
 
-                    else
-                    {
-                        Console.WriteLine($"{userinput} is not a number");
+                        else
+                        {
+                            Console.WriteLine($"{userinput} is not a number");
+                        }
+
                     }
-                    
                 }
-            } while (validResponse == false && userQuit == false);
 
-            Console.WriteLine("end of first loop");
+                Console.WriteLine("end of second loop");
 
-            validResponse = false; //reset flag
-            //Second number
-            while (validResponse == false && userQuit == false)
+                validResponse = false;
+                //Operation Choice
+                while (validResponse == false && userQuit == false)
+                {
+                    validResponse = true; //reset flag
+                    Console.WriteLine("Please select a choice: \n 1. Option 1 \n 2. Option 2 \n 3. Option 3 \n 4. Option 4");
+                    userinput = Console.ReadLine();
+
+                    switch (userinput)
+                    {
+                        case "1":
+                            Console.WriteLine("You have chosen wiseley");
+                            break;
+
+                        case "2":
+                            Console.WriteLine("You have chosen wiseley");
+                            break;
+
+                        case "3":
+                            Console.WriteLine("You have chosen wiseley");
+                            break;
+
+                        case "4":
+                            Console.WriteLine("You have chosen wiseley");
+                            break;
+
+                        case "Q":
+                            userQuit = true;
+                            break;
+
+                        case "q":
+                            userQuit = true;
+                            break;
+
+                        default:
+                            Console.WriteLine("You have chosen Poorly");
+                            validResponse = false;
+                            break;
+                    }
+                }
+
+                Console.WriteLine("end of third loop");
+
+
+            } while (userQuit == false);
             {
-                Console.WriteLine("Please enter a number");
-                userinput = Console.ReadLine();
-                try
-                {
-                    Console.WriteLine($"you entered {int.Parse(userinput)}");
-                    validResponse = true;
-                }
-                catch (Exception)
-                {
-                    if (userinput == "Q" || userinput == "q")
-                    {
-                        userQuit = true;
-                    }
-
-                    else
-                    {
-                        Console.WriteLine($"{userinput} is not a number");
-                    }
-
-                }
+                Console.WriteLine("Have a great day!");
             }
-
-            Console.WriteLine("end of second loop");
-
-            validResponse = false;
-            //Operation Choice
-            while (validResponse == false && userQuit == false)
-            {
-                validResponse = true; //reset flag
-                Console.WriteLine("Please select a choice: \n 1. Option 1 \n 2. Option 2 \n 3. Option 3 \n 4. Option 4");
-                userinput = Console.ReadLine();
-
-                switch (userinput)
-                {
-                    case "1":
-                        Console.WriteLine("You have chosen wiseley");
-                        break;
-
-                    case "2":
-                        Console.WriteLine("You have chosen wiseley");
-                        break;
-
-                    case "3":
-                        Console.WriteLine("You have chosen wiseley");
-                        break;
-
-                    case "4":
-                        Console.WriteLine("You have chosen wiseley");
-                        break;
-
-                    case "Q":
-                        userQuit = true;
-                        break;
-
-                    case "q":
-                        userQuit = true;
-                        break;
-
-                    default:
-                        Console.WriteLine("You have chosen Poorly");
-                        validResponse = false;
-                        break;
-                }
-            }
-
-            Console.WriteLine("end of third loop");
-
-            //pause
-            Console.Read();
+                
+                //pause
+                Console.Read();
         }
     }
 }
