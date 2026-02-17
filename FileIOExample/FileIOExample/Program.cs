5cb5282136ecad2124e403c7ffc6286b2bf0e7b9
Hunter Clezie <clezhunt@gmail.com>
2026-02-17 08:10:12 -0700

    init

---
+++ FileIOExample/FileIOExample/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FileIOExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Pause
+            Console.Read();
+        }
+    }
+}
