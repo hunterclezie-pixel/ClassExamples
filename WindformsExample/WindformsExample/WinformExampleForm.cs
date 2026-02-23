91897e6eb539da9742798404d700e5e9ed5337a4
Hunter Clezie <clezhunt@gmail.com>
2026-02-23 08:18:44 -0700

    rename

---
+++ WindformsExample/WindformsExample/WinformExampleForm.cs
@@ -0,0 +1,15 @@
+namespace WindformsExample
+{
+    public partial class WinformExampleForm : Form
+    {
+        public WinformExampleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
