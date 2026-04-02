34515eaca04f0ffc901145000dfb5a86dbb082e9
Hunter Clezie <clezhunt@gmail.com>
2026-04-02 08:45:45 -0600

    add draw function

--- GraphicsExample/GraphicsExample/GraphicExample.cs
+++ GraphicsExample/GraphicsExample/GraphicExample.cs
@@ -7,6 +7,25 @@ namespace GraphicsExample
             InitializeComponent();
         }
 
+        // Custom methods-------------------------------------------------------------------------
+
+        void DrawLine()
+        {
+            //create a graphics object named g that draws on the picture box
+            Graphics g = DisplayPictureBox.CreateGraphics();
+            //create a pen to draw with
+            Pen thePen = new Pen(Color.Black);
+            thePen.Width = 10;
+            //draw the line here
+            g.DrawLine(thePen, 0, 0, DisplayPictureBox.Width, DisplayPictureBox.Height);
+
+            //free up resorces
+            g.Dispose();
+            thePen.Dispose();
+        }
+
+        // Event handlers-------------------------------------------------------------------------
+
         private void ExitButton_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -14,7 +33,7 @@ namespace GraphicsExample
 
         private void DrawButton_Click(object sender, EventArgs e)
         {
-            //draw
+            DrawLine();
         }
     }
 }
