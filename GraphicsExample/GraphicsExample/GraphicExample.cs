80cb20d9566ef78fb037c9d2527e0e120db5c9c2
Hunter Clezie <clezhunt@gmail.com>
2026-04-02 08:26:21 -0600

    add picture box, anchor all sides for auto resize

--- GraphicsExample/GraphicsExample/GraphicExample.cs
+++ GraphicsExample/GraphicsExample/GraphicExample.cs
@@ -1,8 +1,8 @@
 namespace GraphicsExample
 {
-    public partial class Form1 : Form
+    public partial class GraphicExample : Form
     {
-        public Form1()
+        public GraphicExample()
         {
             InitializeComponent();
         }
